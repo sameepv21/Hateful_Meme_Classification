4bac243de2505b3c0b18a13125e2ac9f5ebcb2c1
sameepv21 <sameep0112vani@gmail.com>
2023-03-29 16:14:13 +0530

    [fix]: working image segregator

--- scripts/image_segregation.py
+++ scripts/image_segregation.py
@@ -25,17 +25,12 @@ def segregate_images():
                 os.system('mv ' + ROOT_URL + '/' + img + ' ' + ROOT_URL + '/dev')
                 new_path = "dev/" + img.strip('img/') + 'g'
             df['img'] = df['img'].replace(img, new_path)
-        out = df.to_json(orient='records')[1:-1].replace('},{', '} {')
-        print(out)
         if(index == 0):
-            with open('../data/facebook/train.jsonl') as f:
-                f.write(out)
+            df.to_json("../data/facebook/train.jsonl", orient = 'records')
         elif(index == 1):
-            with open('../data/facebook/test.jsonl') as f:
-                f.write(out)
+            df.to_json("../data/facebook/test.jsonl", orient = 'records')
         else:
-            with open('../data/facebook/dev.jsonl') as f:
-                f.write(out)
+            df.to_json("../data/facebook/dev.jsonl", orient = 'records')
 
 def separate_classes():
     os.system("mkdir " + os.path.join(ROOT_URL) + '/train/hateful')
@@ -45,34 +40,31 @@ def separate_classes():
 
     for index, df in enumerate([train_df, dev_df]):
         for img in tqdm(df['img']):
-            label = df[df['id'] == id].iloc[0]['label']
+            label = df[df['img'] == img].iloc[0]['label']
             if index == 0:
                 if label == 0:
                     os.system('mv ' + ROOT_URL + '/' + img + ' ' + ROOT_URL + '/train/non_hateful')
                     new_path = "train/non_hateful/" + img.strip('train/')
                 else:
-                    os.system('mv ' + ROOT_URL + '/train/' + img + ' ' + ROOT_URL + '/train/hateful')
+                    os.system('mv ' + ROOT_URL + '/' + img + ' ' + ROOT_URL + '/train/hateful')
                     new_path = "train/hateful/" + img.strip('train/')
             else:
                 if label == 0:
-                    os.system('mv ' + ROOT_URL + '/dev/' + img + ' ' + ROOT_URL + '/dev/non_hateful')
+                    os.system('mv ' + ROOT_URL + '/' + img + ' ' + ROOT_URL + '/dev/non_hateful')
                     new_path = "dev/non_hateful/" + img.strip('dev/')
                 else:
-                    os.system('mv ' + ROOT_URL + '/dev/' + img + ' ' + ROOT_URL + '/dev/hateful')
+                    os.system('mv ' + ROOT_URL + '/' + img + ' ' + ROOT_URL + '/dev/hateful')
                     new_path = "dev/hateful/" + img.strip('dev/')
             df['img'] = df['img'].replace(img, new_path)
-        out = df.to_json(orient='records')[1:-1].replace('},{', '} {')
         if(index == 0):
-            with open('../data/facebook/train.jsonl') as f:
-                f.write(out)
+            df.to_json("../data/facebook/train.jsonl", orient = 'records')
         elif(index == 1):
-            with open('../data/facebook/test.jsonl') as f:
-                f.write(out)
+            df.to_json("../data/facebook/test.jsonl", orient = 'records')
         else:
-            with open('../data/facebook/dev.jsonl') as f:
-                f.write(out)
+            df.to_json("../data/facebook/dev.jsonl", orient = 'records')
 
 def main():
     segregate_images()
     separate_classes()
+    os.system("rm -rf ../data/facebook/img")
 main()
