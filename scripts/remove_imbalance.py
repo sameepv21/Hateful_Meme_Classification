90a4c740e898ff25b4c0f2d99b44d98c3f905123
sameepv21 <sameep0112vani@gmail.com>
2023-03-31 16:33:05 +0530

    [chore]: SMOTE applied

--- scripts/remove_imbalance.py
+++ scripts/remove_imbalance.py
@@ -2,7 +2,7 @@ import pandas as pd
 from imblearn.over_sampling import SMOTENC
 
 def balance(path):
-    df = pd.read_json(path, lines = True)
+    df = pd.read_json(path)
 
     y = df['label']
     x = df.drop('label', axis = 1)
@@ -10,7 +10,7 @@ def balance(path):
     nm = SMOTENC(random_state = 42, categorical_features = [1,2])
     X_new, y_new = nm.fit_resample(x, y)
     df_new = pd.concat([X_new, y_new], axis = 1)
-    df_new.to_json(path, orient = 'records')
+    df_new.to_json(path)
 
-balance('../data/facebook/train.jsonl')
-balance('../data/facebook/dev.jsonl')
+balance('../data/facebook/train.json')
+balance('../data/facebook/dev.json')
