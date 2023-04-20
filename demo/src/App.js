0373426cab37fe45ef47b61f21236d4952cd2930
sameepv21 <sameep0112vani@gmail.com>
2023-04-20 16:23:20 +0530

    [feat]: added prediction based on output from the api

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,36 +1,57 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [file, setFile] = useState(null);
+  const [image, setImage] = useState(null);
+  const [prediction, setPrediction] = useState('');
 
-  const handleFileUpload = (event) => {
-    setFile(event.target.files[0]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('file', image);
+    axios.post('http://localhost:8000/predict', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+    .then(response => {
+      console.log(response.data);
+      if (response.data.prediction === 0) {
+        setPrediction('Not Hateful');
+      } else {
+        setPrediction('Hateful');
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const formData = new FormData();
-    formData.append('file', file);
-
-    try {
-      const response = await axios.post('http://localhost:8000/predict', formData);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
+  const handleImageChange = (event) => {
+    setImage(event.target.files[0]);
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <form onSubmit={handleSubmit} className="max-w-md mx-auto bg-white p-8 rounded-lg shadow-lg">
+    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
         <div className="mb-4">
-          <label htmlFor="image" className="block text-gray-700 font-bold mb-2">Image</label>
-          <input type="file" id="image" name="image" onChange={handleFileUpload} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="image">
+            Image
+          </label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="image" type="file" onChange={handleImageChange} required/>
         </div>
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Submit</button>
+        <div className="flex items-center justify-between">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+            Submit
+          </button>
+        </div>
       </form>
+      {prediction !== '' && (
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <h1 className="block text-gray-700 font-bold mb-2">Prediction</h1>
+          <p className="text-gray-700">{prediction}</p>
+        </div>
+      )}
     </div>
   );
 }
