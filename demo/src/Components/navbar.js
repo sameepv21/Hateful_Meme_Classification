d7810827dcb8807f5abffa437344095c7d7b26dd
sameepv21 <sameep0112vani@gmail.com>
2023-05-05 22:23:33 +0530

    feat: navbar and project added

---
+++ demo/src/Components/navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 py-4">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between">
+          <div className="flex">
+            <Link
+              to="/"
+              className="flex items-center text-white font-bold text-xl uppercase"
+            >
+              My Website
+            </Link>
+          </div>
+          <div className="flex">
+            <Link
+              to="/"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Home
+            </Link>
+            <Link
+              to="/projects"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Projects
+            </Link>
+            <Link
+              to="/docs"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Docs
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
