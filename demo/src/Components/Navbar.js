1f39a0580f5c74ef4215fbc63f3e016211eec8b4
sameepv21 <sameep0112vani@gmail.com>
2023-05-09 19:40:23 +0530

    [style]: renamed to methodology

--- demo/src/Components/Navbar.js
+++ demo/src/Components/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
               to="/architecture"
               className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
             >
-              Architecture
+              Methodology
             </Link>
           </div>
         </div>
