dd91115308757df5190ddc45abae2e9488655378
sameepv21 <sameep0112vani@gmail.com>
2023-05-09 21:14:17 +0530

    [style]: project page completed

--- demo/src/Components/Project.js
+++ demo/src/Components/Project.js
@@ -14,17 +14,17 @@ function App() {
         'Content-Type': 'multipart/form-data'
       }
     })
-    .then(response => {
-      console.log(response.data);
-      if (response.data.prediction === 0) {
-        setPrediction('Not Hateful');
-      } else {
-        setPrediction('Hateful');
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    });
+      .then(response => {
+        console.log(response.data);
+        if (response.data.prediction === 0) {
+          setPrediction('Not Hateful');
+        } else {
+          setPrediction('Hateful');
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   const handleImageChange = (event) => {
@@ -32,26 +32,32 @@ function App() {
   };
 
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="image">
-            Image
-          </label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="image" type="file" onChange={handleImageChange} required/>
-        </div>
-        <div className="flex items-center justify-between">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-            Submit
-          </button>
-        </div>
-      </form>
-      {prediction !== '' && (
-        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <h1 className="block text-gray-700 font-bold mb-2">Prediction</h1>
-          <p className="text-gray-700">{prediction}</p>
-        </div>
-      )}
+    <div className='bg-gray-100'>
+      <div className='flex justify-center mt-5 text-4xl font-bold text-violet-800'>
+        Bring your vision to life - upload and test your images now
+      </div>
+      <div className="min-h-screen flex flex-col justify-center items-center">
+        
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="image">
+              Image
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="image" type="file" onChange={handleImageChange} required />
+          </div>
+          <div className="flex items-center justify-between">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+        {prediction !== '' && (
+          <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <h1 className="block text-gray-700 font-bold mb-2">Prediction</h1>
+            <p className="text-gray-700">{prediction}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
